16b0c9043b07ad8d82363e7a14d6f338c8e38659
gdi2290 <github@gdi2290.com>
2013-09-23 17:27:33 -0700

    init tests

---
+++ test/test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// var angular-momentjs = require('../lib/angular-momentjs.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['angular-momentjs'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(1, 1, 'should be 1.');
+    test.done();
+  }
+};
