7aec6ec6014701d09c071b18b6eb6dd25eb39e69
gdi2290 <github@gdi2290.com>
2013-09-23 17:59:02 -0700

    initial moment wrapper for src

--- angular-momentjs.js
+++ angular-momentjs.js
@@ -2,5 +2,32 @@
   'use strict';
 
 angular.module('angular-momentjs', [])
+  .factory('$moment', ['$rootScope', '$document', '$q', '$window',
+    function($rootScope, $document, $q, $window) {
+      var d = $q.defer();
+      var moment = $window.moment;
+      function onScriptLoad() {
+        // Load client in the browser
+        $rootScope.$apply(function() {
+          d.resolve($window.d3);
+        });
+      }
+      // Create a script tag with moment as the source
+      // and call our onScriptLoad callback when it
+      // has been loaded
+      var scriptTag = $document[0].createElement('script');
+      scriptTag.type = 'text/javascript';
+      scriptTag.async = true;
+      scriptTag.src = '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.2.1/moment.min.js';
+      scriptTag.onreadystatechange = function () {
+        if (this.readyState == 'complete') onScriptLoad();
+      }
+      scriptTag.onload = onScriptLoad;
+
+      var s = $document[0].getElementsByTagName('body')[0];
+      s.appendChild(scriptTag);
+      moment.promise = d.promise;
+      return moment;
+  }]);
 
 }());
