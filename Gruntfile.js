d639145381c24330f27e790b5415cd2d71336f3b
gdi2290 <github@gdi2290.com>
2013-09-23 17:26:33 -0700

    add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['angular-pubnub.js', 'example/main.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      }
+    },
+    complexity: {
+      generic: {
+        src: ['angular-momentjs.js'],
+        options: {
+          jsLintXML: 'report.xml', // create XML JSLint-like report
+          checkstyleXML: 'checkstyle.xml', // create checkstyle report
+          errorsOnly: false, // show only maintainability errors
+          cyclomatic: 3,
+          halstead: 8,
+          maintainability: 100
+        }
+      }
+    }
+  });
+
+  // Test task
+  grunt.registerTask('test', [
+    'jshint',
+    'nodeunit',
+    'complexity'
+  ]);
+
+  // Default task.
+  grunt.registerTask('default', [
+    'jshint',
+    'nodeunit',
+    'complexity'
+  ]);
+
+};
