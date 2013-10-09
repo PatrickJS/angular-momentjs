6ca610f82d874c873429980c2c9d65034e4b780b
PatrickJS <github@gdi2290.com>
2013-10-09 16:52:03 -0700

    update moment with angular-moment for now

--- angular-momentjs.js
+++ angular-momentjs.js
@@ -26,8 +26,94 @@ angular.module('angular-momentjs', [])
 
       var s = $document[0].getElementsByTagName('body')[0];
       s.appendChild(scriptTag);
+
       moment.promise = d.promise;
       return moment;
+  }])
+  .directive('amTimeAgo', ['$moment', '$timeout', function ($moment, $timeout) {
+
+    return function (scope, element, attr) {
+      var activeTimeout = null;
+      var currentValue;
+      var currentFormat;
+
+      function cancelTimer() {
+        if (activeTimeout) {
+          $timeout.cancel(activeTimeout);
+          activeTimeout = null;
+        }
+      }
+
+      function updateTime(momentInstance) {
+        element.text(momentInstance.fromNow());
+        var howOld = $moment().diff(momentInstance, 'minute');
+        var secondsUntilUpdate = 3600;
+        if (howOld < 1) {
+          secondsUntilUpdate = 1;
+        } else if (howOld < 60) {
+          secondsUntilUpdate = 30;
+        } else if (howOld < 180) {
+          secondsUntilUpdate = 300;
+        }
+
+        activeTimeout = $timeout(function () {
+          updateTime(momentInstance);
+        }, secondsUntilUpdate * 1000, false);
+      }
+
+      function updateMoment() {
+        cancelTimer();
+        updateTime($moment(currentValue, currentFormat));
+      }
+
+      scope.$watch(attr.amTimeAgo, function (value) {
+        if ((typeof value === 'undefined') || (value === null) || (value === '')) {
+          cancelTimer();
+          if (currentValue) {
+            element.text('');
+            currentValue = null;
+          }
+          return;
+        }
+
+        if (angular.isNumber(value)) {
+          // Milliseconds since the epoch
+          value = new Date(value);
+        }
+        // else assume the given value is already a date
+
+        currentValue = value;
+        updateMoment();
+      });
+
+      attr.$observe('amFormat', function (format) {
+        currentFormat = format;
+        if (currentValue) {
+          updateMoment();
+        }
+      });
+
+      scope.$on('$destroy', function () {
+        cancelTimer();
+      });
+    };
+  }])
+  .filter('amDateFormat', ['$moment', function ($moment) {
+
+    return function (value, format) {
+      if (typeof value === 'undefined' || value === null) {
+        return '';
+      }
+
+      if (!isNaN(parseFloat(value)) && isFinite(value)) {
+        // Milliseconds since the epoch
+        value = new Date(parseInt(value, 10));
+      }
+      // else assume the given value is already a date
+
+      return $moment(value).format(format);
+    };
   }]);
+  
 
 }());
