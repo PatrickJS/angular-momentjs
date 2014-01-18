1782b413ec04f6dadadc6da4916d60814e887163
gdi2290 <github@gdi2290.com>
2014-01-18 10:35:22 -0700

    update Gruntfile with build task and live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,121 @@
 'use strict';
 
-module.exports = function (grunt) {
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+var mountFolder = function(connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
 
-  // Project configuration.
   grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js']
+    pkg: grunt.file.readJSON('package.json'),
+    bwr: grunt.file.readJSON('bower.json'),
+    concat: {
+      dist:{}
     },
-    jshint: {
+    ngmin: {
+      dist: {}
+    },
+    uglify: {
       options: {
-        jshintrc: '.jshintrc'
+        report: 'min',
+        enclose: {
+          'this': 'window',
+          'this.angular': 'angular',
+          'void 0': 'undefined'
+        },
+        banner: '/*\n  <%= pkg.name %> - v<%= pkg.version %> \n  ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %>\n*/\n'+
+        '',
       },
-      gruntfile: {
-        src: 'Gruntfile.js'
+      dist: {
+        options: {
+          beautify: false,
+          mangle: true,
+          compress: {
+            global_defs: {
+              'DEBUG': false
+            },
+            dead_code: true
+          },
+          sourceMap: '<%= bwr.name %>.min.js.map'
+        },
+        files: {
+          '<%= bwr.name %>.min.js': ['./lib/index.js', './lib/*/*.js']
+        }
       },
-      lib: {
-        src: ['angular-momentjs.js']
-      },
-      test: {
-        src: ['test/**/*.js']
+      src: {
+        options: {
+          beautify: true,
+          mangle: false,
+          compress: false
+        },
+        files: {
+          '<%= bwr.name %>.js': ['./lib/index.js', './lib/*/*.js']
+        }
       }
     },
     watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          'example/{,*/}*.html',
+          'example/{,*/}*.js',
+          '{,*/}*.js'
+        ]
       }
     },
-    complexity: {
-      generic: {
-        src: ['angular-momentjs.js'],
+    connect: {
+      options: {
+        port: '3000',
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost'
+      },
+      livereload: {
         options: {
-          jsLintXML: 'report.xml', // create XML JSLint-like report
-          checkstyleXML: 'checkstyle.xml', // create checkstyle report
-          errorsOnly: false, // show only maintainability errors
-          cyclomatic: 3,
-          halstead: 8,
-          maintainability: 100
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'example'),
+              mountFolder(connect, '.')
+            ];
+          }
         }
       }
+
     }
+
   });
 
-  // Test task
+  grunt.registerTask('server', function (target) {
+    grunt.task.run([
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
   grunt.registerTask('test', [
-    'jshint',
-    'nodeunit',
-    'complexity'
   ]);
 
-  // Default task.
+  grunt.registerTask('build', [
+    'concat',
+    'ngmin',
+    'uglify'
+  ]);
+
   grunt.registerTask('default', [
-    'jshint',
-    'nodeunit',
-    'complexity'
+    'build'
   ]);
-
 };
